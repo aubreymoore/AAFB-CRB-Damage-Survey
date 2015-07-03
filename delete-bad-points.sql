638e3b4a7f7afda5d9d715edd68e5c1aa3bfa9a2
Aubrey Moore <aubreymoore@guam.net>
2015-07-04 07:42:47 +1000

    First commit - lots of unnecessary files

---
+++ delete-bad-points.sql
@@ -0,0 +1,8 @@
+delete from crb_tree_inspection where crb_tree_inspection_key = 'eb5524e0-5ec7-4475-c72c-a8bc3eb5898d';
+delete from crb_tree_inspection where crb_tree_inspection_key = '3d1121fc-1f26-49b5-bffc-206f73e655ad';
+delete from crb_tree_inspection where crb_tree_inspection_key = '459a8a40-29a8-43d9-9f0a-c1e58256f208';
+delete from crb_tree_inspection where crb_tree_inspection_key = '12ba211b-907f-4eea-9e73-ab9ab9086673';
+delete from crb_tree_inspection where crb_tree_inspection_key = '9cc1836a-3587-4887-a6d8-3040c1c0f6f3';
+delete from crb_tree_inspection where crb_tree_inspection_key = '3e0dd388-0954-4ea8-8422-0b9520bc07da';
+delete from crb_tree_inspection where crb_tree_inspection_key = '1ff13119-2688-4f5e-b424-1ad9844ae46a';
+
